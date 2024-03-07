d017b6c511924c57275679fb9731ae35d0779879
dmuthuraaj <dhanamuthu8@gmail.com>
2024-03-07 16:42:50 +0530

    Missed imporatant thing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -18,4 +19,7 @@ func main() {
 	r := gin.Default()
 	routerGroup := r.Group("/api/v1")
 	routerGroup.GET(uriHome, HomeHandler)
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("unable to start server: %v", err)
+	}
 }
